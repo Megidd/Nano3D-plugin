82ab8465b15becb365ca996fcbf818619eed4d7d
Megidd Git <megiddgit@gmail.com>
2023-04-20 01:58:57 -0700

    New command by:
    https://developer.rhino3d.com/guides/rhinocommon/adding-commands-to-projects/

---
+++ Nano3D/Nano3DHollowing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Nano3D
+{
+    public class Nano3DHollowing : Command
+    {
+        public Nano3DHollowing()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static Nano3DHollowing Instance { get; private set; }
+
+        public override string EnglishName => "Nano3DHollowing";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
