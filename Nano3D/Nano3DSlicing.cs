9bd2109671f28f368004e3a5131085411941f0a8
Megidd Git <megiddgit@gmail.com>
2023-04-20 02:28:32 -0700

    New command:
    https://developer.rhino3d.com/guides/rhinocommon/adding-commands-to-projects/

---
+++ Nano3D/Nano3DSlicing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Nano3D
+{
+    public class Nano3DSlicing : Command
+    {
+        public Nano3DSlicing()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static Nano3DSlicing Instance { get; private set; }
+
+        public override string EnglishName => "Nano3DSlicing";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
