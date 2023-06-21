6fa9049ccfb8cbc32f9a7f60866e0f5590c30c56
Megidd Git <megiddgit@gmail.com>
2023-06-21 12:50:53 +0330

    New command

---
+++ Nano3D/FiniteElementAnalysis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Nano3D
+{
+    public class FiniteElementAnalysis : Command
+    {
+        public FiniteElementAnalysis()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static FiniteElementAnalysis Instance { get; private set; }
+
+        public override string EnglishName => "FiniteElementAnalysis";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
